af2e4391bce0b7fbb9480be8f9f8e6a771f5f16c
SteveTheSpaceFan <yuyustevejobs@icloud.com>
2021-06-23 11:21:33 -0700

    Show/hide button update

--- displayComponents/dispUpdate.js
+++ displayComponents/dispUpdate.js
@@ -129,10 +129,10 @@ if (typeof window.orientation == 'undefined') {
 
 function show_hideEngineControl() {
     if (showedEngineControl) {
-        document.getElementById("engineControl").style.transform = "translate(-95%, 0)"
+        document.getElementById("engineControl").style.transform = "translate(-92%, 0)"
         document.getElementById("engineControl").style.flexDirection = "row";
     } else {
-        document.getElementById("engineControl").style.transform = "translate(0, 0)"
+        document.getElementById("engineControl").style.transform = "translate(-10%, 0)"
         document.getElementById("engineControl").style.flexDirection = "row-reverse";
     }
 
