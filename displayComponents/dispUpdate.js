3cb66e8c6ff9984feb2802495dcccdcfc013b137
SteveTheSpaceFan <yuyustevejobs@icloud.com>
2021-07-02 12:36:04 -0700

    fix

--- displayComponents/dispUpdate.js
+++ displayComponents/dispUpdate.js
@@ -6,28 +6,32 @@ function updateFlightParamDisp() {
         } else {
             document.getElementById("altitudeDisp").textContent = (altitude * 0.001).toFixed(1) + "KM";
         }
-        //propellent
-        document.getElementById("propellantMassDisp").textContent = (propellantMass * 0.001).toFixed(0);
+
         //speed
         if (trueSpeed < 1000) {
             document.getElementById("speedDisp").textContent = trueSpeed.toFixed(0) + "M/S";
         } else {
             document.getElementById("speedDisp").textContent = (trueSpeed * 0.001).toFixed(1) + "KM/S";
         }
-        //twr
-        document.getElementById("twrDisp").textContent = twr.toFixed(1);
-        //speedX
-        document.getElementById("speedX").textContent = Math.ceil(speedX);
-        //speedY
-        document.getElementById("speedY").textContent = Math.ceil(speedY);
-        //g
-        document.getElementById("gforece").textContent = (totalAcceleration / gravity).toFixed(1);
-        //ToSite
-        let disToSite = Math.ceil(downRangeDistance - starBaseXpos)
-        if (disToSite < 1000 && disToSite > -1000) {
-            document.getElementById("distanceToLandingSite").textContent = disToSite + " m";
-        } else {
-            document.getElementById("distanceToLandingSite").textContent = (disToSite * 0.001).toFixed(1) + " km";
+
+        if (showedFlightParamDispMid) {
+            //propellent
+            document.getElementById("propellantMassDisp").textContent = (propellantMass * 0.001).toFixed(0);
+            //twr
+            document.getElementById("twrDisp").textContent = twr.toFixed(1);
+            //speedX
+            document.getElementById("speedX").textContent = Math.ceil(speedX);
+            //speedY
+            document.getElementById("speedY").textContent = Math.ceil(speedY);
+            //g
+            document.getElementById("gforece").textContent = (totalAcceleration / gravity).toFixed(1);
+            //ToSite
+            let disToSite = Math.ceil(downRangeDistance - starBaseXpos)
+            if (disToSite < 1000 && disToSite > -1000) {
+                document.getElementById("distanceToLandingSite").textContent = disToSite + " m";
+            } else {
+                document.getElementById("distanceToLandingSite").textContent = (disToSite * 0.001).toFixed(1) + " km";
+            }
         }
 
         //restartButton
